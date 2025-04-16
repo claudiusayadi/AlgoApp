91db18cddebb8899ccca8867349290b36981d8f2
Claudius A. <ayadiclaudius@gmail.com>
2025-04-16 17:29:26 +0100

    Add 02Factorial class library for calculating factorial of numbers

---
+++ 02Factorial/Class1.cs
@@ -0,0 +1,23 @@
+﻿namespace _02Factorial;
+
+/// <summary>
+/// Provides methods to calculate factorials of numbers.
+/// </summary>
+public class FactorialCalculator
+{
+	/// <summary>
+	/// Calculates the factorial of a number.
+	/// </summary>
+	/// <param name="n">The number to calculate factorial for.</param>
+	/// <returns>The factorial of n.</returns>
+	public static int Calculate(int n)
+	{
+		int factorial = 1;
+		for (int i = 1; i <= n; i++)
+		{
+			factorial *= i;
+		}
+
+		return factorial;
+	}
+}
