4b853f4ed556ecf874ff2074c2eacfbb365373cd
Claudius A. <ayadiclaudius@gmail.com>
2025-04-16 17:26:09 +0100

    Add 01NaturalSum class library for calculating sum of natural numbers

---
+++ 01NaturalSum/Class1.cs
@@ -0,0 +1,23 @@
+﻿namespace _01NaturalSum;
+
+/// <summary>
+/// Provides methods to calculate the sum of natural numbers.
+/// </summary>
+public class NaturalNumberCalculator
+{
+    /// <summary>
+    /// Calculates the sum of first n natural numbers.
+    /// </summary>
+    /// <param name="n">The number of natural numbers to sum.</param>
+    /// <returns>The sum of first n natural numbers.</returns>
+    public static int CalculateSum(int n)
+    {
+        int sum = 0;
+        for (int i = 0; i <= n; i++)
+        {
+            sum += i;
+        }
+
+        return sum;
+    }
+}
