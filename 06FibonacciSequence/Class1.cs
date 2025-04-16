6816b8fde1ca9011b9fae8cf110fd277cd10525c
Claudius A. <ayadiclaudius@gmail.com>
2025-04-16 17:31:11 +0100

    Add 06FibonacciSequence class library for generating Fibonacci sequences

---
+++ 06FibonacciSequence/Class1.cs
@@ -0,0 +1,33 @@
+﻿namespace _06FibonacciSequence;
+
+/// <summary>
+/// Provides methods to work with Fibonacci sequences.
+/// </summary>
+public class FibonacciGenerator
+{
+    /// <summary>
+    /// Generates the first n terms of the Fibonacci sequence.
+    /// </summary>
+    /// <param name="n">The number of terms to generate.</param>
+    /// <returns>An array containing the first n terms of the Fibonacci sequence.</returns>
+    public static int[] GenerateSequence(int n)
+    {
+        List<int> sequence = [];
+        int n1 = 0;
+        int n2 = 1;
+        int nth;
+
+        sequence.Add(n1);
+        sequence.Add(n2);
+
+        for (int i = 2; i < n; i++)
+        {
+            nth = n1 + n2;
+            sequence.Add(nth);
+            n1 = n2;
+            n2 = nth;
+        }
+
+        return sequence.ToArray();
+    }
+}
