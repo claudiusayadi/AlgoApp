71daf7cea64a970328883c104aa9924fbf55b32d
Claudius A. <ayadiclaudius@gmail.com>
2025-04-16 17:30:16 +0100

    Add 04EvenNumbers class library for finding even numbers in a range

---
+++ 04EvenNumbers/Class1.cs
@@ -0,0 +1,26 @@
+﻿namespace _04EvenNumbers;
+
+/// <summary>
+/// Provides methods to work with even numbers.
+/// </summary>
+public class EvenNumberFinder
+{
+	/// <summary>
+	/// Gets all even numbers from 1 to n.
+	/// </summary>
+	/// <param name="n">The upper limit.</param>
+	/// <returns>An array of even numbers from 1 to n.</returns>
+	public static int[] GetEvenNumbers(int n)
+	{
+		List<int> evenNumbers = [];
+		for (int i = 1; i <= n; i++)
+		{
+			if (i % 2 == 0)
+			{
+				evenNumbers.Add(i);
+			}
+		}
+
+		return evenNumbers.ToArray();
+	}
+}
